41d9236a8b9a99673031724d2a157c6d4b5cb453
shw98 <67368023+DASHorRUSH@users.noreply.github.com>
2022-04-18 23:10:25 +0900

    04.18

---
+++ chap02/src/src02/exam05/Alarm.java
@@ -0,0 +1,20 @@
+package src02.exam05;
+import java.util.Scanner;
+
+public class Alarm {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int H = scanner.nextInt();
+		int M = scanner.nextInt();
+		
+		if( M - 45 >= 0 ) {
+			System.out.println(H + " " + M);
+		} else {
+			if() {
+				
+			}
+		}
+	}
+
+}
