51af2e04f6adde2dcbe08a67b72ba0cc8a399ec4
SHW98 <sywok19980331@gmail.com>
2022-05-04 16:08:44 +0900

    05.04

--- chap03/src/src03/exam09/Star2.java
+++ chap03/src/src03/exam09/Star2.java
@@ -6,6 +6,17 @@ public class Star2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		for(int i=0; i<N; i++) {
+			for(int j=1; j<N-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
-
 }
+
+//
---
+++ chap03/src/src03/exam10/Xmin.java
@@ -0,0 +1,12 @@
+package src03.exam10;
+import java.util.Scanner;
+
+public class Xmin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+	}
+
+}
