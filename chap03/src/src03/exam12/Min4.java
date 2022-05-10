831892ada393c2896a880ea0286a2233e938f290
SHW98 <sywok19980331@gmail.com>
2022-05-11 04:33:49 +0900

    05.11

--- chap03/src/src03/exam12/Min4.java
+++ chap03/src/src03/exam12/Min4.java
@@ -1,24 +1,22 @@
 package src03.exam12;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Min4 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		try {
-			while(true) {
-				int A = scanner.nextInt();
-				int B = scanner.nextInt();
-				int result = A + B;
-				System.out.println(result);
-			}
-		} catch(NullPointerException n) {
-			System.out.println("catch");
-		} finally {
-			System.out.println("finally");
-			scanner.close();
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String str= " ";
+		
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str);
+			sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
 		}
+		System.out.print(sb);
 	}
-
 }
---
+++ chap03/src/src03/exam13/Cycle.java
@@ -0,0 +1,11 @@
+package src03.exam13;
+import java.util.Scanner;
+
+public class Cycle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);	
+
+	}
+}
