a7fa0fb995613a03d316b84d7be29bc00b1447a7
SHW98 <sywok19980331@gmail.com>
2022-07-23 21:51:49 +0900

    07.23

---
+++ chap06/src/src06/exam01/ASCII.java
@@ -0,0 +1,15 @@
+package src06.exam01;
+
+import java.util.Scanner;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		
+	      Scanner scanner = new Scanner(System.in);
+	      char str = scanner.nextLine().charAt(0);
+	      int num = (int)str;
+	      System.out.println(num);
+	}
+
+}
