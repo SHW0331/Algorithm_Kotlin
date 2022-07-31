f487b7fb829b71a9c244bf877f8b157fa9958672
SHW98 <sywok19980331@gmail.com>
2022-08-01 01:19:49 +0900

    07.30

--- chap06/src/src06/exam08/Dial.java
+++ chap06/src/src06/exam08/Dial.java
@@ -7,22 +7,28 @@ public class Dial {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		String dial = scanner.next();
-		int uni = 65;
-		int count = 3;
 		int result = 0;
 		
+		
 		for(int i=0; i<dial.length(); i++) {
-			for(int y=2; y<10; y++) {
-				for(int x=65; x<91; x++) {
-					if(dial.charAt(i) == (char)x) {
+			for(int x=65; x<91; x++) {
+				if(dial.charAt(i) == (char)x) {
+					if(x == 90 || x == 89) {
+						result += 10;
+						break;
+					} else if(x == 83) {
+						result += 8;
+						break;
+					} else if( x == 86){
+						result += 9;
+						break;
+					} else {
 						result += (x - 65) / 3 + 3;  
 						break;
 					}
 				}
-	
 			}
 		}
-		
 		System.out.println(result);
 	}
 }
