ce0d0306465c31de1fb4d9511ddbfe37098d32a8
SHW98 <sywok19980331@gmail.com>
2022-08-01 23:09:11 +0900

    08.01

--- chap06/src/src06/exam09/Cro.java
+++ chap06/src/src06/exam09/Cro.java
@@ -1,11 +1,37 @@
 package src06.exam09;
 
+import java.util.Scanner;
+
 public class Cro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int uni = 65;
-		System.out.println((char)uni);
-
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		int word = 0;
+		
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == '=' && i > 0) {
+				if(str.charAt(i-1) == 'c' || str.charAt(i-1) == 's') {
+					
+				} else if(str.charAt(i-1) == 'z') {
+					if(str.charAt(i-2) == 'd' && (i > 2 && str.length()>2)) {
+						word--;
+					}
+				} else
+					word++;
+			} else if(str.charAt(i) == '-' && i > 0) {
+				if(str.charAt(i-1) == 'c' || str.charAt(i-1) == 'd') {
+					
+				} else
+					word++;
+			} else if(str.charAt(i) == 'j' && i > 0) {
+				if(str.charAt(i-1) == 'l' || str.charAt(i-1) == 'n') {
+					
+				} else
+					word++;
+			} else
+				word++;
+		}
+		System.out.println(word);
 	}
 }
---
+++ chap06/src/src06/exam10/group.java
@@ -0,0 +1,18 @@
+package src06.exam10;
+
+import java.util.Scanner;
+
+public class group {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.next();
+		int word = 0;
+
+		
+		for(int i=0; i<str.length(); i++) {
+			
+		}
+	}
+
+}
