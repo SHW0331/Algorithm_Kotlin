5913e057380ef94962721822870f4eb3f785d8dc
SHW98 <sywok19980331@gmail.com>
2022-08-03 00:02:54 +0900

    08.02

--- chap06/src/src06/exam10/group.java
+++ chap06/src/src06/exam10/group.java
@@ -6,9 +6,52 @@ public class group {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.next();
-		int word = 0;
-		
-		
+		int num = scanner.nextInt();
+		String arr[] = new String[num];
+
+		int result = 0;
+
+		for (int i = 0; i < num; i++) {
+			arr[i] = scanner.next();
+			String delete = "";
+			int count[] = new int[26];
+			int check = 0;
+
+			for (int y = 0; y < arr[i].length(); y++) {
+				if (y == arr[i].length() - 1 && arr[i].length() > 1) {
+					if (arr[i].charAt(y) == arr[i].charAt(y - 1)) {
+
+					} else {
+						delete += arr[i].charAt(y);
+					}
+				} else if (arr[i].length() == 1) {
+					delete += arr[i].charAt(y);
+				} else {
+					if (arr[i].charAt(y) == arr[i].charAt(y + 1)) {
+
+					} else {
+						delete += arr[i].charAt(y);
+					}
+				}
+			}
+
+			for (int x = 0; x < delete.length(); x++) {
+				for (int z = 97; z < 123; z++) {
+					if (delete.charAt(x) == (char) z) {
+						count[z - 97] += 1;
+					}
+				}
+			}
+
+			for (int k = 0; k < count.length; k++) {
+				if (count[k] > 1) {
+					check = 0;
+					break;
+				} else
+					check = 1;
+			}
+			result += check;
+		}
+		System.out.println(result);
 	}
 }
---
+++ chap07/src/src07/exam01/breakEven.java
@@ -0,0 +1,29 @@
+package src07.exam01;
+
+import java.util.Scanner;
+
+public class breakEven {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		String arr[] = new String[num];
+		int result = 0;
+		
+		for(int i=0; i<num; i++) {
+			String str = scanner.next();
+			arr[i] = str;
+		}
+		
+		for(int i=0; i<arr[i].length(); i++) {
+			if(arr[i].length() == 1)
+				result++;
+			else if(
+					) {
+				
+			} else {
+				
+			}
+		}
+	}
+}
