2c19c58c4d4a5af9d43d592c9dbe4d794e4fcd69
SHW98 <sywok19980331@gmail.com>
2022-08-04 01:41:26 +0900

    08.03

--- chap06/src/src06/exam10/group.java
+++ chap06/src/src06/exam10/group.java
@@ -7,50 +7,25 @@ public class group {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
-		String arr[] = new String[num];
-
 		int result = 0;
 
 		for (int i = 0; i < num; i++) {
-			arr[i] = scanner.next();
+			String str = scanner.next();
 			String delete = "";
-			int count[] = new int[26];
-			int check = 0;
+			boolean check[] = new boolean[26];
+			boolean tmp = true;
 
-			for (int y = 0; y < arr[i].length(); y++) {
-				if (y == arr[i].length() - 1 && arr[i].length() > 1) {
-					if (arr[i].charAt(y) == arr[i].charAt(y - 1)) {
-
-					} else {
-						delete += arr[i].charAt(y);
+			for (int y = 0; y < str.length(); y++) {
+				int index = str.charAt(y) - 'a';
+				if(check[index]) {
+					if(str.charAt(y) != str.charAt(y-1)) {
+						tmp = false;
+						break;
 					}
-				} else if (arr[i].length() == 1) {
-					delete += arr[i].charAt(y);
-				} else {
-					if (arr[i].charAt(y) == arr[i].charAt(y + 1)) {
-
-					} else {
-						delete += arr[i].charAt(y);
-					}
-				}
+ 				} else
+					check[index] = true;
 			}
-
-			for (int x = 0; x < delete.length(); x++) {
-				for (int z = 97; z < 123; z++) {
-					if (delete.charAt(x) == (char) z) {
-						count[z - 97] += 1;
-					}
-				}
-			}
-
-			for (int k = 0; k < count.length; k++) {
-				if (count[k] > 1) {
-					check = 0;
-					break;
-				} else
-					check = 1;
-			}
-			result += check;
+			if(tmp) result++;
 		}
 		System.out.println(result);
 	}
--- chap07/src/src07/exam02/honey.java
+++ chap07/src/src07/exam02/honey.java
@@ -5,52 +5,29 @@ import java.util.Scanner;
 public class honey {
 
 	public static void main(String[] args) {
-	      Scanner scanner = new Scanner(System.in);
-	      int num = scanner.nextInt();
-	      int result = 0;
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		int result = 0;
 
-	      for (int i = 0; i < num; i++) {
-	         String str = scanner.next();
-	         int arr[] = new int[26];
-	         int check = 0;
+		for (int i = 0; i < num; i++) {
+			String str = scanner.next();
+			String delete = "";
+			boolean check[] = new boolean[26];
+			boolean tmp = true;
 
-	         for (int y = 0; y < str.length(); y++) {
-	            if (str.length() == 1) {
-	               check++;
-	               break;
-	            } else if (y == str.length() - 1) {
-	               if (str.charAt(y) == str.charAt(y - 1)) {
-
-	               } else {
-	                  for (int x = 97; x < 123; x++) {
-	                     if (str.charAt(y) == (char) x) {
-	                        arr[x - 97] += 1;
-	                     }
-	                  }
-	               }
-	            } else { //
-	               for (int x = 97; x < 123; x++) {
-	                  if (str.charAt(y) == (char) x) {
-	                     if (str.charAt(y) == str.charAt(y + 1)) {
-
-	                     } else {
-	                        arr[x - 97] += 1;
-	                     }
-	                  }
-	               }
-	            }
-
-	            for (int k = 0; k < arr.length; k++) {
-	               if (arr[k] > 1) {
-	                  check = 0;
-	                  break;
-	               } else
-	                  check = 1;
-	            }
-	         }
-	         result += check;
-	      }
-	      System.out.println(result);
+			for (int y = 0; y < str.length(); y++) {
+				int index = str.charAt(y) - 'a';
+				if(check[index]) {
+					if(str.charAt(y) != str.charAt(y-1)) {
+						tmp = false;
+						break;
+					}
+ 				} else
+					check[index] = true;
+			}
+			if(tmp) result++;
+		}
+		System.out.println(result);
 	}
 
 }
