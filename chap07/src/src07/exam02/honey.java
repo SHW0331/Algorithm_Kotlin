ee1068da94af0180c1185d345e2418ca5b477f53
SHW98 <sywok19980331@gmail.com>
2022-08-05 00:49:45 +0900

    08.04

--- chap07/src/src07/exam02/honey.java
+++ chap07/src/src07/exam02/honey.java
@@ -6,28 +6,18 @@ public class honey {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		int result = 0;
-
-		for (int i = 0; i < num; i++) {
-			String str = scanner.next();
-			String delete = "";
-			boolean check[] = new boolean[26];
-			boolean tmp = true;
-
-			for (int y = 0; y < str.length(); y++) {
-				int index = str.charAt(y) - 'a';
-				if(check[index]) {
-					if(str.charAt(y) != str.charAt(y-1)) {
-						tmp = false;
-						break;
-					}
- 				} else
-					check[index] = true;
-			}
-			if(tmp) result++;
-		}
-		System.out.println(result);
+        int num = scanner.nextInt();
+        int door = 1;
+        int d = 0;
+        
+        while(num > door) {
+            door = door + 6*d;
+            d++;
+        }
+        if(num == 1) {
+            d = 1;
+        }
+        System.out.println(d);
 	}
 
 }
