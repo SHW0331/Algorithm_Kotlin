73567871aca5c62084b26dc26f93b36eb4519708
SHW98 <sywok19980331@gmail.com>
2022-08-07 18:23:43 +0900

    08.06

--- chap07/src/src07/exam04/Snail.java
+++ chap07/src/src07/exam04/Snail.java
@@ -1,13 +1,22 @@
 package src07.exam04;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Snail {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-
+		int day = (V - B) / (A - B);
+		if ((V - B) % (A - B) != 0) {
+			day++;
+		}
+		System.out.println(day);
 	}
-
 }
