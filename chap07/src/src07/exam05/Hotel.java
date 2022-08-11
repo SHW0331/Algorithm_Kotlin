54662977f82fae48abc8e74d52603a9bbd95dcf5
SHW98 <sywok19980331@gmail.com>
2022-08-11 23:42:27 +0900

    08.11

---
+++ chap07/src/src07/exam05/Hotel.java
@@ -0,0 +1,12 @@
+package src07.exam05;
+
+import java.util.Scanner;
+
+public class Hotel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner= new Scanner(System.in);
+	}
+
+}
