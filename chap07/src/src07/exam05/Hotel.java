88a1f055fbd3426729262d32ed5cb07f4f6bba79
SHW98 <sywok19980331@gmail.com>
2022-08-17 01:03:37 +0900

    08.17

--- chap07/src/src07/exam05/Hotel.java
+++ chap07/src/src07/exam05/Hotel.java
@@ -5,8 +5,37 @@ import java.util.Scanner;
 public class Hotel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner= new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+		int arr[] = new int[T];
+		for (int i = 0; i < T; i++) {
+			int H = scanner.nextInt();
+			int W = scanner.nextInt();
+			int N = scanner.nextInt();
+			int resultW = 0;
+			int resultH = 0;
+			int ans = 0;
+
+			if (N % H == 0) {
+				if (N / H > 0) {
+					resultW = N / H;
+					resultH = H;
+					arr[i] = resultH * 100 + resultW;
+				} else {
+					resultW = 1;
+					resultH = N;
+					arr[i] = resultH * 100 + resultW;
+				}
+			} else {
+				resultW = N / H + 1;
+				resultH = N % H;
+				arr[i] = resultH * 100 + resultW;
+			}
+		}
+
+		for (int i = 0; i < T; i++) {
+			System.out.println(arr[i]);
+		}
 	}
 
 }
