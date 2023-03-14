423eff1973fff0de4bb6a6e712b48da1eb126282
bite98 <sywok19980331@gmail.com>
2023-03-15 05:33:40 +0900

    03.15

--- Algorithm Study/Algorithm/src/main/kotlin/Main.kt
+++ Algorithm Study/Algorithm/src/main/kotlin/Main.kt
@@ -9,28 +9,86 @@ fun main(args: Array<String>) {
     var (n, m, v) = br.readLine().split(' ').map { it.toInt() }
 
     // 간선이 연결하는 두 정점의 번호
-    val linkArr = Array(m){ IntArray(2)}
-    for(i in 0 until m) {
+    val linkArr = Array(m) { IntArray(2) }
+    for (i in 0 until m) {
         val input = br.readLine().split(' ').map { it.toInt() }
-        for(j in 0 until 2) {
-            linkArr[i][j] = input[j]
+        for (j in 0 until 2) {
+            linkArr[i][j] = input.sorted()[j]
+            /*
+            ----- 1 ------
+            00  01 = 1 2   sorted
+            10  11 = 1 3
+            20  21 = 1 4
+            30  31 = 2 4
+            40  41 = 3 4
+
+            ------ 2 ------
+            00  01 = 5 4 --> 4 5
+            10  11 = 5 2 --> 2 5
+            20  21 = 1 2 --> 1 2
+            30  31 = 3 4 --> 3 4
+            40  41 = 3 1 --> 1 3
+            */
         }
     }
 
     // 큐 생성
     var arr = mutableListOf<Int>(v)
+    /*
+    index 0번째 꺼내서 간선 추가하기
+    1. 시작 정점에 해당하는 배열 index를 찾고 오름차순으로 정렬
+    2. 큐, 순서 결과에 추가
+    3. 두 번째 정점에서 이미 존재하는 정점 제외한 나머지 간선 찾고, 오름차순으로 정렬
+    4. 큐, 순서 결과에 추가
+    5. ... 반복
+    */
 
-    // 순서 결과
-    var order = mutableListOf<Int>()
+    // 간선이 1개일 경우
+    if (m == 1) {
 
-    // index 0번째 꺼내서 간선 추가하기
-    for(i in 0 until n) {
-        when {
-            linkArr[v][0] > linkArr[v][1] -> {
-                arr[0] = linkArr[v][0]
-                order.add(v)
+        if (linkArr[0][0] == v) {
+            println("${v} ${linkArr[0][1]}")
+        } else {
+            println("${v} ${linkArr[0][0]}")
+        }
+
+
+    }
+    // 간선이 여러개 이상
+    else {
+        for (i in 0 until n) {
+            if (i == 0) {
+                var find = mutableListOf<Int>()
+                for (j in 0 until m) {
+                    if (arr[i] == linkArr[j][0]) {
+                        find.add(linkArr[j][1])
+                    } else if (arr[i] == linkArr[j][1]) {
+                        find.add(linkArr[j][0])
+                    }
+                }
+                arr += find.sorted().toMutableList()
+
+            } else {
+                var find = mutableListOf<Int>()
+                for (j in 0 until m) {
+                    if (arr[i] == linkArr[j][0]) {
+                        if (arr.contains(linkArr[j][1])) {
+
+                        } else {
+                            find.add(linkArr[j][1])
+                        }
+                    } else if (arr[i] == linkArr[j][1]) {
+                        if (arr.contains(linkArr[j][0])) {
+
+                        } else {
+                            find.add(linkArr[j][0])
+                        }
+                    }
+                }
+                arr += find.sorted().toMutableList()
             }
         }
+        println(arr)
+
     }
-    
 }
